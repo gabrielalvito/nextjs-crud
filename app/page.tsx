46759b871120b56c168f33832e58c14e1445ff2a
Gabriel Alvito <alvitoadinugraha@gmail.com>
2023-08-04 14:45:05 +0700

    design update

--- app/page.tsx
+++ app/page.tsx
@@ -18,34 +18,41 @@ async function getProducts() {
 export default async function ProductList() {
   const products: Product[] = await getProducts();
   return (
-    <div className="px-20 py-20 overflow-x-auto">
-      <h3 className="text-center text-base font-bold">Simple CRUD using Next.js</h3>
-      <div className="py-2">
-        <AddProduct />
+    <div className="px-20 py-20">
+      <div className="card lg bg-base-100 shadow-xl">
+        <div className="card-body">
+        <h2 className="card-title font-bold justify-center">CRUD using Next.js and json-server</h2>
+          <div className="py-2">
+            <AddProduct />
+          </div>
+
+          <table className="table mt-3 table-lg">
+            <thead>
+              <tr className="text-base text-center bg-base-200">
+                <th>No</th>
+                <th>Product Name</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <tr key={product.id} className="text-center">
+                  <td>{index + 1}</td>
+                  <td>{product.title}</td>
+                  <td>{product.price}</td>
+                  <td className="flex justify-center gap-1">
+                    <UpdateProduct{...product} />
+                    <DeleteProduct{...product} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <table className="table mt-5">
-        <thead>
-          <tr className="text-base text-center bg-base-200">
-            <th>No</th>
-            <th>Product Name</th>
-            <th>Price</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product, index) => (
-            <tr key={product.id} className="text-center">
-              <td>{index + 1}</td>
-              <td>{product.title}</td>
-              <td>{product.price}</td>
-              <td className="flex justify-center gap-1">
-                <UpdateProduct{...product} />
-                <DeleteProduct{...product} />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
     </div>
+
   )
 }
